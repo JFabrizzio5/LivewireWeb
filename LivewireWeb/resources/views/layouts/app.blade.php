3033e5b86c8c375a2535d097310a371351cb3162
Joseph Fabrizzio <115313375+JFabrizzio5@users.noreply.github.com>
2024-08-16 14:58:38 -0600

    Ejemplo contador livewirevolt
    Se añade un layout y un laravel volt para testear

---
+++ LivewireWeb/resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!-- resources/views/layouts/app.blade.php -->
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Mi Aplicación')</title>
+    <!-- Aquí puedes incluir tus estilos -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+<body>
+    <header>
+        <!-- Aquí puedes poner el header -->
+
+    </header>
+    <main>
+        @yield('content')
+    </main>
+    <footer>
+        <!-- Aquí puedes poner el footer -->
+
+    </footer>
+    <!-- Aquí puedes incluir tus scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+
+</html>
--- LivewireWeb/resources/views/pages/index.blade.php
+++ LivewireWeb/resources/views/pages/index.blade.php
@@ -1,3 +1,40 @@
+@extends('layouts.app')
+
+@section('title', 'Inicio deportivo')
+
+<?php
+
+use function Livewire\Volt\{state};
+
+state(['count' => 0]);
+
+$increment = fn() => $this->count++;
+
+?>
 <div>
-    //
+
+
+    @section('content')
+        @volt
+            <div>
+                <h1>Bienvenido a la página de inicio</h1>
+                <p>Este es el contenido de la página de inicio.</p>
+
+                <H1>Hola que tal este es el index</H1>
+
+
+                <h1>{{ $count }}</h1>
+                <button wire:click="increment">+</button>
+            </div>
+        @endvolt
+    @endsection
+
+    <div>
+        <H1>Hola que tal este es el index</H1>
+    </div>
+
+
+
+
+
 </div>
